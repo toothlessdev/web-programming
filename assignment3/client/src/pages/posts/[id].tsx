d0222925b86b3df3175fe89b0ac5e8455fa0c135
toothlessdev <toothless042@gmail.com>
2024-04-01 03:16:45 +0900

    글 상세 페이지 ServerSideRendering 적용

--- assignment3/client/src/pages/posts/[id].tsx
+++ assignment3/client/src/pages/posts/[id].tsx
@@ -1,49 +1,59 @@
-import { GetServerSidePropsContext, NextPage } from "next";
+import { postService } from "@/services/post.service";
+import styles from "@/styles/NewPostPage.module.css";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
 
-export default function PostDetailPage(props) {
-  return (
-    <>
-      <main>
-        <div>
-          <h2>{props.title}</h2>
-          <p>글쓴이 : {props.author}</p>
-          <p>작성시간 : {props.createdAt}</p>
-          <p>내용</p>
-          <p className="content">{props.content}</p>
-        </div>
-      </main>
-
-      <style jsx>{`
-        div {
-          width: min(100%, 600px);
-          margin: 0px auto;
-          padding: 20px 0px;
-        }
-        div > h2 {
-          font-size: 1.2rem;
-          font-weight: bold;
-        }
-        .content {
-          margin: 10px 0px;
-          border: 1px solid #fff;
-          border-radius: 10px;
-          padding: 10px;
-        }
-      `}</style>
-    </>
-  );
+export interface PageProps {
+    title: string;
+    author: string;
+    content: string;
+    createdAt: string;
 }
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { id } = context.query;
+export default function PostDetailPage(props: PageProps) {
+    const router = useRouter();
 
-  const request = async () => {
-    const response = await fetch(process.env.API_BASE + `/posts/${id}`);
-    if (!response.ok) throw new Error();
-    return response.json();
-  };
+    const onEditBtnClicked = () => {
+        router.push(`/posts/edit/${router.query.id}`);
+    };
+    const onDeleteBtnClicked = () => {
+        postService.deletePost(Number(router.query.id)).then(() => {
+            router.push(`/`);
+        });
+    };
 
-  const data = await request();
+    return (
+        <main className={styles["main"]}>
+            <h2>글 상세정보</h2>
 
-  return { props: data };
+            <div className={styles["input-item"]}>
+                <label htmlFor="title">글 제목</label>
+                <input id="title" type="text" value={props.title} />
+            </div>
+
+            <div className={styles["input-item"]}>
+                <label htmlFor="author">글쓴이</label>
+                <input id="author" type="text" value={props.author} />
+            </div>
+
+            <div className={styles["input-item"]}>
+                <label htmlFor="content">내용</label>
+                <textarea name="content" id="content" cols={30} rows={10} value={props.content}></textarea>
+            </div>
+
+            <div className={styles["input-item"]} style={{ display: "flex", gap: "10px" }}>
+                <button onClick={onEditBtnClicked}>수정하기</button>
+                <button onClick={onDeleteBtnClicked}>삭제하기</button>
+            </div>
+        </main>
+    );
 }
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async (context: GetServerSidePropsContext) => {
+    const { id } = context.query;
+    const { title, author, content, createdAt } = await postService.readPostById(id);
+
+    return {
+        props: { title, author, content, createdAt },
+    };
+};
