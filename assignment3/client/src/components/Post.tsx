9d4974b516ab8303593048d2f57668d61645dba2
toothlessdev <toothless042@gmail.com>
2024-04-01 03:15:21 +0900

    상세 게시글 정보로 이동하도록 설정

--- assignment3/client/src/components/Post.tsx
+++ assignment3/client/src/components/Post.tsx
@@ -1,14 +1,22 @@
+import { useRouter } from "next/router";
 import styles from "./Post.module.css";
 
 export interface IPost {
+    id: number;
     title: string;
     author: string;
     createdAt: string;
 }
 
-export const Post: React.FC<IPost> = ({ title, author, createdAt }) => {
+export const Post: React.FC<IPost> = ({ id, title, author, createdAt }) => {
+    const router = useRouter();
+
     return (
-        <div className={styles["post-wrapper"]}>
+        <div
+            className={styles["post-wrapper"]}
+            onClick={() => {
+                router.push(`/posts/${id}`);
+            }}>
             <h3>{title}</h3>
             <p>글쓴이 : {author}</p>
             <p>작성시간 : {createdAt}</p>
