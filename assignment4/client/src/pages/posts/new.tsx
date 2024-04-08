c587c702de0d978675e23056ec8889184674b824
toothlessdev <toothless042@gmail.com>
2024-04-08 13:49:41 +0900

    이미지 업로드 기능 구현

--- assignment4/client/src/pages/posts/new.tsx
+++ assignment4/client/src/pages/posts/new.tsx
@@ -1,4 +1,4 @@
-import { api } from "@/services/api";
+import { API_BASE_URL, api } from "@/services/api";
 import { postService } from "@/services/post.service";
 import styles from "@/styles/NewPostPage.module.css";
 import { useRouter } from "next/router";
@@ -10,17 +10,31 @@ export default function NewPostPage() {
     const titleRef = useRef<HTMLInputElement>(null);
     const authorRef = useRef<HTMLInputElement>(null);
     const contentRef = useRef<HTMLTextAreaElement>(null);
+    const imageRef = useRef<HTMLInputElement>(null);
 
     const onClick = () => {
-        postService
-            .createPost({
-                title: titleRef.current?.value as string,
-                author: authorRef.current?.value as string,
-                content: contentRef.current?.value as string,
-            })
-            .then(() => {
+        const formData = new FormData();
+        const image = imageRef.current?.files && imageRef.current?.files[0];
+        if (image) formData.append("image", image);
+
+        formData.append("title", titleRef.current?.value as string);
+        formData.append("author", authorRef.current?.value as string);
+        formData.append("content", contentRef.current?.value as string);
+
+        fetch(API_BASE_URL + "/posts", {
+            method: "POST",
+            // headers: {
+            //     "Content-Type": "multipart/form-data",
+            // },
+            body: formData,
+        })
+            .then((res) => {
                 alert("글이 성공적으로 등록되었습니다");
                 router.replace("/");
+            })
+            .catch((err) => {
+                console.log(formData);
+                alert("글 등록에 실패하였습니다");
             });
     };
 
@@ -41,6 +55,11 @@ export default function NewPostPage() {
             <div className={styles["input-item"]}>
                 <label htmlFor="content">내용</label>
                 <textarea name="content" id="content" cols={30} rows={10} ref={contentRef}></textarea>
+            </div>
+
+            <div className={styles["input-item"]}>
+                <label htmlFor="content">이미지 업로드</label>
+                <input type="file" accept="image/jpg, image/jpeg, image/png" ref={imageRef} />
             </div>
 
             <div className={styles["input-item"]}>
